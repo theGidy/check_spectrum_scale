68264f694bc229f6f100715a9d6ddd651d0ebea8
Posovszky <Philipp.Posovszky@dlr.de>
2016-08-31 13:03:02 +0200

    change checksum

---
+++ check_spectrum_scale.py
@@ -0,0 +1,42 @@
+!/usr/bin/python
+################################################################################
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+################################################################################
+
+################################################################################
+# Name:    Check IBM Spectrum Scale / GPFS
+# Author:  Philipp Posovszky, DLR - philipp.posovszky@gmail.com
+# Version: 0.0.1
+# Date: 30/08/2016
+# Dependencies:
+#   - IBM Spectrum Scale
+################################################################################
+
+
+# The actual code is managed in the following Git rebository - please use the
+# Issue Tracker to ask questions, report problems or request enhancements. The
+# repository also contains an extensive README.
+#   https://github.com/theGidy/check_spectrum_scale.git
+
+
+################################################################################
+# Disclaimer: This sample is provided 'as is', without any warranty or support.
+# It is provided solely for demonstrative purposes - the end user must test and
+# modify this sample to suit his or her particular environment. This code is
+# provided for your convenience, only - though being tested, there's no
+# guarantee that it doesn't seriously break things in your environment! If you
+# decide to run it, you do so on your own risk!
+################################################################################
+
+
+################################################################################
+## Variable definition
+################################################################################
+
+
+sys.exit(0)
+
+
