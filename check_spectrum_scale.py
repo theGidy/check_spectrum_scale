cbc767ec152331d4bb1fabef7c460db230305237
poso_ph <philipp.posovszky@gmail.com>
2017-01-09 08:02:03 +0100

    Bugfix Blocksize check
    There was a problem with calling the -d paramter to check the blocksize.
    Also the performancedata was wrong for this check.

--- check_spectrum_scale.py
+++ check_spectrum_scale.py
@@ -296,7 +296,7 @@ def checkFileSets(args):
     if args.filesets:
         command += " " + args.filesets
     if args.size:
-        command += "-d"
+        command += " -d"
     command += " -Y"
    
     output = executeBashCommand(command)
@@ -343,7 +343,7 @@ def checkFileSets(args):
             checkResult.longOutput += "Warning FileSets: " + ", ".join(warningNodeUtilization) + "\n"
         checkResult.performanceData = ""
         for x in resultList:
-            checkResult.performanceData += x.filesetName + "=" + str(x.freeInodes) + ";" + str(calculatePercentageOfValue(args.warning, x.maxInodes)) + ";" + str(calculatePercentageOfValue(args.critical, x.maxInodes)) + ";0;" + str(x.maxInodes) + " "+x.filesetName+"_blockSiz:"+str(x.dataSize)+"KB;;;; ";
+            checkResult.performanceData += x.filesetName + "=" + str(x.freeInodes) + ";" + str(calculatePercentageOfValue(args.warning, x.maxInodes)) + ";" + str(calculatePercentageOfValue(args.critical, x.maxInodes)) + ";0;" + str(x.maxInodes) + " "+x.filesetName+"_blockSiz="+str(x.dataSize)+"KB;;;; ";
             
     elif args.link:
         linkedList=[x.filesetName for x in resultList if x.status == 'Linked']
